54fd1f0ca8c5572e7bb0296f7e6c1a249166591f
Bu Yong Tatt <48663882+buyongtatt@users.noreply.github.com>
2020-12-31 09:17:12 +0800

    Add files via upload

---
+++ src/Figure 1.java
@@ -0,0 +1,34 @@
+//class A extends Runnable {
+//    @Override
+//    public void run() {
+//
+//    }
+//}
+//class B extends Object{
+//    public void run() {
+//
+//    }
+//}
+//
+//class C implements Runnable{
+//    @Override
+//    public void run() {
+//
+//    }
+//}
+//
+//class D implements Runnable {
+//    void run() {
+//
+//    }
+//}
+//
+//class E implements Runnable{
+//    public void start() {
+//
+//    }
+//}
+//
+//
+//
+//
